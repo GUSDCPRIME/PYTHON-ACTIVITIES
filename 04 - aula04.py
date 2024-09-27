233fba0b7126f6c707d8fe9d5777fca1874c8c85
GUSPRIME <158516104+GUSDCPRIME@users.noreply.github.com>
2024-09-26 22:58:05 -0300

    Add files via upload

---
+++ 04 - aula04.py
@@ -0,0 +1,30 @@
+# operadores aritméticos:
+v1 = float(input('digite o primeiro valor: '))
+v2 = float(input('digite o segundo valor: '))
+# Soma: 
+soma = v1 + v2
+print(f'A soma =', soma)
+
+# subtração
+sub = v1 - v2
+print(f'A subtração é =', sub)
+
+# multiplicação:
+mult = v1 * v2
+print(f'A multiplicação é =', mult)
+
+# divisão:
+div = v1 / v2
+print(f'A divisão é =', div)
+
+# Divisão Inteira:
+div_inteira = v1 // v2
+print(f'A divisão inteira é =', div_inteira)
+
+# Resto da divisão:
+resto = v1 % v2
+print(f'O resto da divisão é =', resto)
+
+# exponenciação:
+exp = v1 ** v2
+print(f'A exponenciação é =', exp)
