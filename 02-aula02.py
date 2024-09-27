f78ae2b7451e90133dccb73ded6aa768fabe3f99
GUSPRIME <158516104+GUSDCPRIME@users.noreply.github.com>
2024-09-26 22:57:06 -0300

    Add files via upload

---
+++ 02-aula02.py
@@ -0,0 +1,12 @@
+# atribuição de variáveis
+nome = "Gustavo"
+idade = 17
+altura = 1.75
+
+# exibir mensagem na tela
+print(" nome: ", nome, " idade: ", idade, " altura: ", altura)
+# formatação com f-string
+print(f"nome: {nome} idade: {idade} altura: {altura}")
+
+nome = input("Digite o seu nome: ")
+print("nome: ", nome) #imprime o nome digitado pelo usuário
