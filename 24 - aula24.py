2cf64f5d753afdfda1f58030daf8b414256ca21b
GUSPRIME <158516104+GUSDCPRIME@users.noreply.github.com>
2024-11-07 18:21:01 -0300

    Add files via upload

---
+++ 24 - aula24.py
@@ -0,0 +1,11 @@
+# um algoritmo que calcula a soma dos números pares entre 1 e 50
+
+soma = 0 
+
+# utilizando o laço for:
+
+for numero in range(1,51):
+    if numero % 2 == 0:
+        soma += numero 
+
+print(f" a soma dos números pares entre 1 e 50 é: {soma}.")
